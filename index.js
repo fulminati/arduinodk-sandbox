bae4b29992789e50db5f3f4ebae4c3efa8651474
Francesco Bianco <info.francescobianco@gmail.com>
2018-09-24 14:35:50 +0200

    Update from 0.0.15

--- index.js
+++ index.js
@@ -7,102 +7,54 @@
 
 const fu = require('nodejs-fu')
     , cliz = require('cliz')
-    , glob = require('glob')
     , foreach = require('boor').foreach
+    , http = require('http')
     , EOL = require('os')
 
 module.exports = {
 
     /**
-     * USE this tag\(("|')(.*?[^\\])??((\\\\)+)?+("|') for escaoe quotes
-     *
+     * Create new server.
      */
-    selector: function (filter) {
-        return new RegExp('@' + filter + '\\(([^]*?)\\)', 'gm')
-    },
-
-    /**
-     *
-     * @param sketch
-     * @param filter
-     * @param processSelector
-     */
-    onBefore: function (sketch, filter, processor) {
-        var files = glob.sync('**/*.{ino,h}', { cwd: sketch.path, absolute: true })
-        var selector = this.selector(filter)
-
-        foreach(files, (file) => {
-            var prev = file + '.' + filter
-            var meta = fu.readFile(fu.fileExists(prev) ? prev : file)
-
-            if (meta.match(selector)) {
-                var code = this.processor(file, meta, selector, processor)
-                fu.writeFile(prev, meta)
-                fu.writeFile(file, code)
-            }
+    createServer: function () {
+        let carier = {}
+        let routes = {}
+        let port = 9001
+        let server = http.createServer((req, res) => {
+            console.log(req.url)
+            carier = { status: 404, contentType: 'text/html', body: '<h1>Page not found</h1>' }
+            if (typeof routes[req.url] == 'function') { let cb = routes[req.url]; cb(); }
+            res.writeHead(carier.status, {'Content-Type': carier.contentType});
+            res.write(carier.body);
+            res.end();
         });
-    },
 
-    /**
-     *
-     * @param sketch
-     * @param filter
-     */
-    onAfter: function (sketch, filter) {
-        var files = glob.sync('**/*.' + filter, { cwd: sketch.path, absolute: true })
-
-        foreach(files, (file) => {
-            fu.writeFile(file.slice(0, -1 - filter.length), fu.readFile(file))
-            fu.unlink(file)
-        });
-    },
-
-    /**
-     *
-     * @param code
-     * @param processor
-     */
-    processor: function (file, code, selector, processor) {
-        return code.replace(selector, function (token, args) {
-            let left = code.substr(0, code.indexOf(token))
-            let row = (left.match(/\n/g) || []).length + 1;
-            let col = left.length - left.lastIndexOf('\n')
-
-            let I = '[-+]?\\d+'
-            let F = '[-+]?\\d+\\.\\d'
-            let L = '[a-zA-Z_]+\\w*'
-            let S = '("(?:[^"\\\\]|\\\\.)*")'
-            let Q = '(\'(?:[^\'\\\\]|\\\\.)*\')'
-            let V = `(${I}|${F}|${L}|${S}|${Q})`
-
-            if (!args.match(new RegExp(`^\\s*${V}(\\s*,\\s*${V})*\\s*$`, 'm'))) {
-                cliz.fatal(`Filter syntax error at '${file}' line '${row}'.`);
+        server.listen(port, (err) => {
+            if (err) {
+                return console.log('something bad happened', err)
             }
 
-            let tokenizer = new RegExp(`\\s*${V}\\s*`, 'gm')
-            let value = null;
-            let values = [];
-            while (true) {
-                value = tokenizer.exec(args);
-                if (!value) { break; }
-                else if (value[1].match(new RegExp(`^${I}$`, 'gm'))) { value = parseInt(value[1]); }
-                else if (value[1].match(new RegExp(`^${F}$`, 'gm'))) { value = parseFloat(value[1]); }
-                else if (value[1].match(new RegExp(`^${L}$`, 'gm'))) { value = value[1]; }
-                else if (value[1].match(new RegExp(`^${S}$`, 'gm'))) { value = value[1].slice(1, -1).replace(/\\"/gm, '"'); }
-                else if (value[1].match(new RegExp(`^${Q}$`, 'gm'))) { value = value[1].slice(1, -1).replace(/\\'/gm, "'"); }
-                values.push(value)
-            }
-
-            return processor(values)
+            console.log(`server is listening on ${port}`)
         })
+
+        server.on = (route, cb) => { routes[route] = cb }
+
+        server.send = (status, contentType, body) => {
+            carier.status = status
+            carier.contentType = contentType
+            carier.body = body
+        }
+
+        return server
     },
 
     /**
+     * Read file from local system.
      *
-     * @param string
-     * @returns {string}
+     * @param file
+     * @returns {*}
      */
-    quote: function (string) {
-        return '"' + string + '"';
+    readFile: function (file) {
+        return fu.readFile(file)
     }
 };
